9341d62f31d95566d053974590757bc94d20e721
JosePedro <159468247+JosePedro228@users.noreply.github.com>
2026-05-07 19:44:30 +0200

    #46

---
+++ Java/HistoriasHardware/src/main/java/com/mycompany/historiashardware/GestorAlmacen.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.historiashardware;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+/**
+ *
+ * @author DAM113
+ */
+public class GestorAlmacen {
+    private static final String url = "jdbc:mysql://localhost:3306/mi_basedatos";
+    private static final String usuario = "root";
+    private static final String password = "mysql";
+    
+     
+}
