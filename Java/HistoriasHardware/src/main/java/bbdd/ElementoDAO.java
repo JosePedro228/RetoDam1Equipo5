98d22135da3dcd96b4e726d55ea75530fa865eef
M. <cuentamaya@gmail.com>
2026-05-11 21:48:15 +0200

    #61  clase elementoDAO
    metodos insert, update, delete, y busqueda de inventario: completo, por nombre, por tipo, por estado y por ubicacion

--- Java/HistoriasHardware/src/main/java/bbdd/ElementoDAO.java
+++ Java/HistoriasHardware/src/main/java/bbdd/ElementoDAO.java
@@ -5,6 +5,7 @@
 package bbdd;
 
 import com.mycompany.historiashardware.Elemento;
+import com.mycompany.historiashardware.Ubicacion;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -45,7 +46,7 @@ public class ElementoDAO {
                 if (resultado == 0) {
 
                     resultado = -1;
-                    
+
                 } else {//si no falla entonces resultado = 0
 
                     resultado = 0;
@@ -88,10 +89,233 @@ public class ElementoDAO {
 
         } catch (SQLException ex) {
 
+            Logger.getLogger(ElementoDAO.class.getName()).
+                    log(Level.SEVERE, null, ex);
         }
 
         return resultado;
     }
     
+    public static int eliminarElemento(Connection con, int id){
+    
+        int resultado = -1;
+        PreparedStatement ps = null;
+
+        String s = "DELETE FROM ELEMENTOS WHERE ID_ELEMENTO = ?";
+
+        try {
+
+            ps = con.prepareStatement(s);
+
+            ps.setInt(1, id);
+
+            resultado = ps.executeUpdate();
+
+            if (resultado == 0) {
+
+                resultado = -1;
+            } else {
+
+                resultado = 0;
+            }
+
+        } catch (SQLException ex) {
+
+            Logger.getLogger(ElementoDAO.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+
+        return resultado;
+    }
+
+    //REVISAR METODO -> GUARDAR UBICACIONES, ID 
+    public static ArrayList<String> listarInventario(Connection con) {
+
+        //variables
+        ArrayList<String> resultado = new ArrayList<>();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String cadena = "SELECT ID, NOMBRE, DESCRIPCION, TIPO, ESTADO, UBICACION"
+                + "FROM ELEMENTOS";
+
+        try {
+
+            //preparar consulta
+            ps = con.prepareStatement(cadena);
+
+            //ejecutar consulta
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                int id = rs.getInt(1);
+                String nombre = rs.getString(2);
+                String desc = rs.getString(3);
+                String tipo = rs.getString(4);
+                String estado = rs.getString(5);
+                int ubi = rs.getInt(6);
+
+                //añadir resultados a la lista
+                resultado.add(id + " " + nombre + " " + desc + " " + tipo + " " + estado + " " + ubi);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ElementoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return resultado;
+
+    }
+
+    public static ArrayList<String> listarInventarioNombre(Connection con, String nom) {
+        //variables
+        ArrayList<String> resultado = new ArrayList<>();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String cadena = "SELECT ID, NOMBRE, DESCRIPCION, TIPO, ESTADO, UBICACION"
+                + "FROM ELEMENTOS WHERE NOMBRE = ?";
+
+        try {
+
+            //preparar consulta
+            ps = con.prepareStatement(cadena);
+            ps.setString(1, nom);
+
+            //ejecutar consulta
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                int id = rs.getInt(1);
+                String nombre = rs.getString(2);
+                String desc = rs.getString(3);
+                String tipo = rs.getString(4);
+                String estado = rs.getString(5);
+                int ubi = rs.getInt(6);
+
+                //añadir resultados a la lista
+                resultado.add(id + " " + nombre + " " + desc + " " + tipo + " " + estado + " " + ubi);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ElementoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return resultado;
+    }
+    
+    public static ArrayList<String> listarInventarioTipo(Connection con, String t){
+    
+         //variables
+        ArrayList<String> resultado = new ArrayList<>();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String cadena = "SELECT ID, NOMBRE, DESCRIPCION, TIPO, ESTADO, UBICACION"
+                + "FROM ELEMENTOS WHERE TIPO = ?";
+
+        try {
+
+            //preparar consulta
+            ps = con.prepareStatement(cadena);
+            ps.setString(1, t);
+
+            //ejecutar consulta
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                int id = rs.getInt(1);
+                String nombre = rs.getString(2);
+                String desc = rs.getString(3);
+                String tipo = rs.getString(4);
+                String estado = rs.getString(5);
+                int ubi = rs.getInt(6);
+
+                //añadir resultados a la lista
+                resultado.add(id + " " + nombre + " " + desc + " " + tipo + " " + estado + " " + ubi);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ElementoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return resultado;
+    }
+    
+    public static ArrayList<String> listarInventarioEstado(Connection con, String s){
+    
+         //variables
+        ArrayList<String> resultado = new ArrayList<>();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String cadena = "SELECT ID, NOMBRE, DESCRIPCION, TIPO, ESTADO, UBICACION"
+                + "FROM ELEMENTOS WHERE ESTADO = ?";
+
+        try {
+
+            //preparar consulta
+            ps = con.prepareStatement(cadena);
+            ps.setString(1, s);
+
+            //ejecutar consulta
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                int id = rs.getInt(1);
+                String nombre = rs.getString(2);
+                String desc = rs.getString(3);
+                String tipo = rs.getString(4);
+                String estado = rs.getString(5);
+                int ubi = rs.getInt(6);
+
+                //añadir resultados a la lista
+                resultado.add(id + " " + nombre + " " + desc + " " + tipo + " " + estado + " " + ubi);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ElementoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return resultado;
+    }
+    
+    public static ArrayList<String> listarInvetarioUbicacion(Connection con, int ubicacion){
+    
+         //variables
+        ArrayList<String> resultado = new ArrayList<>();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String cadena = "SELECT ID, NOMBRE, DESCRIPCION, TIPO, ESTADO, UBICACION"
+                + "FROM ELEMENTOS WHERE UBICACION = ?";
+
+        try {
+
+            //preparar consulta
+            ps = con.prepareStatement(cadena);
+            ps.setInt(1, ubicacion);
+
+            //ejecutar consulta
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                int id = rs.getInt(1);
+                String nombre = rs.getString(2);
+                String desc = rs.getString(3);
+                String tipo = rs.getString(4);
+                String estado = rs.getString(5);
+                int ubi = rs.getInt(6);
+
+                //añadir resultados a la lista
+                resultado.add(id + " " + nombre + " " + desc + " " + tipo + " " + estado + " " + ubi);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ElementoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return resultado;
+    }
 
 }
