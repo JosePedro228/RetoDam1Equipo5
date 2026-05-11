346b38fa4dfeb66cc0f99632b5a9f29f2141ec30
Miguel Martínez <mmartinezr2402@educantabria.es>
2026-05-11 16:03:07 +0200

    Update UsuariosDAO.java

--- Java/HistoriasHardware/src/main/java/bbdd/UsuariosDAO.java
+++ Java/HistoriasHardware/src/main/java/bbdd/UsuariosDAO.java
@@ -3,38 +3,67 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package bbdd;
+
 import java.sql.Connection;
 import java.sql.*;
 import Usuarios.Usuario;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author DAM116
  */
 public class UsuariosDAO {
-    
+
     Set<Usuario> listaUsuarios;
 
     public UsuariosDAO() {
-        this.listaUsuarios=new HashSet<Usuario>();
+        this.listaUsuarios = new HashSet<Usuario>();
     }
-    
-    public void insertar(Usuario u){
-        
+
+    public static void insertar(Connection con, Usuario u) {
+        int resultado = -1;
+        PreparedStatement ps = null;
+        if(u!=null && !buscar(con, u)){
+            String sql ="INSERT INTO usuarios (id_usuario, nombre, contraseña, id_rol) VALUES (?,?,?,?)";
+            try{
+                ps=con.prepareStatement(sql);
+                
+                
+                
+            }catch(SQLException e){
+                Logger.getLogger(UsuariosDAO.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+
     }
-    
-    public void modificar(){
-        
+
+    public static void modificar() {
+
     }
-    
-    public void buscar(Usuario u){
-        
+
+    public static boolean buscar(Connection con, Usuario u) {
+        boolean resultado = true;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String sql = "SELECT * FROM usuarios WHERE id_usuario = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, u.getId_usuario());
+            rs=ps.executeQuery();
+            resultado=rs.next();
+        } catch (SQLException e) {
+            Logger.getLogger(UsuariosDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return resultado;
     }
-    
-    public void borrar(){
-        
+
+    public void borrar() {
+
     }
-    
+
 }
