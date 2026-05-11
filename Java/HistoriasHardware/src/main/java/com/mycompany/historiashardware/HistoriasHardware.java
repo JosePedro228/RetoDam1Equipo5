452e5755f040aa21195aa9b6405f54ff286cf800
Miguel Martínez <mmartinezr2402@educantabria.es>
2026-05-11 18:18:59 +0200

    asf

--- Java/HistoriasHardware/src/main/java/com/mycompany/historiashardware/HistoriasHardware.java
+++ Java/HistoriasHardware/src/main/java/com/mycompany/historiashardware/HistoriasHardware.java
@@ -4,13 +4,18 @@
 package com.mycompany.historiashardware;
 
 import Swing.Interfaz;
+import Usuarios.Administrador;
+import Usuarios.Profesor;
+import Usuarios.Usuario;
 import bbdd.ConnectionDB;
 import bbdd.GestorAlmacenDAO;
 import bbdd.UbicacionDAO;
+import bbdd.UsuariosDAO;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashSet;
 import java.util.List;
 import javax.swing.SwingUtilities;
 
@@ -23,12 +28,26 @@ public class HistoriasHardware {
     public static void main(String[] args) throws SQLException {
         
           Connection con = ConnectionDB.openConnection();
-          // UbicacionDAO.insertar(con);
+           UbicacionDAO.insertar(con);
           
            List<Elemento> inv = GestorAlmacenDAO.devolverInventarioCompleto(con);
            for(Elemento e: inv){
                System.out.println(e);
            }
+           Administrador a=new Administrador("Miguel","miguel25","miguel2525",1);
+           Profesor p=new Profesor("Saúl","saul25","saul2525",2);
+           System.out.println("Insertar: ");
+           UsuariosDAO.insertar(con, a);
+           UsuariosDAO.insertar(con, p);
+           System.out.println("Mostrar: ");
+           HashSet<Usuario> lista=UsuariosDAO.devolverUsuarios(con);
+           for (Usuario usuario : lista) {
+               System.out.println(usuario.toString());
+        }
+           System.out.println("Borrar: ");
+           UsuariosDAO.borrar(con, a.getId_usuario());
+           UsuariosDAO.borrar(con, p.getId_usuario());
+           System.out.println("Fin: ");
         /*SwingUtilities.invokeLater(() -> {
 
             Interfaz interfaz = new Interfaz();
