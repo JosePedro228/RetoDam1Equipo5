302b05c3422e30ee7ee37623b8d6a2df004ef7f5
M. <cuentamaya@gmail.com>
2026-05-12 17:17:56 +0200

    Conexión de ventanas básicas de la app
    inicio de app con el login (sin validar acceso), conexion de ventanas para inventario, informes y prestamos

--- Java/HistoriasHardware/src/main/java/com/mycompany/historiashardware/HistoriasHardware.java
+++ Java/HistoriasHardware/src/main/java/com/mycompany/historiashardware/HistoriasHardware.java
@@ -3,7 +3,8 @@
  */
 package com.mycompany.historiashardware;
 
-import Swing.Interfaz;
+
+import Swing.Swing;
 import Usuarios.Administrador;
 import Usuarios.Profesor;
 import Usuarios.Usuario;
@@ -26,31 +27,31 @@ public class HistoriasHardware {
 
     public static void main(String[] args) throws SQLException {
         
-          Connection con = ConnectionDB.openConnection();
-           UbicacionDAO.insertar(con);
-          
-           List<Elemento> inv = GestorAlmacenDAO.devolverInventarioCompleto(con);
-           for(Elemento e: inv){
-               System.out.println(e);
-           }
-           Administrador a=new Administrador("Miguel","miguel25","miguel2525",1);
-           Profesor p=new Profesor("Saúl","saul25","saul2525",2);
-           System.out.println("Insertar: ");
-           UsuariosDAO.insertar(con, a);
-           UsuariosDAO.insertar(con, p);
-           System.out.println("Mostrar: ");
-           HashSet<Usuario> lista=UsuariosDAO.devolverUsuarios(con);
-           for (Usuario usuario : lista) {
-               System.out.println(usuario.toString());
-        }
-           System.out.println("Borrar: ");
-           UsuariosDAO.borrar(con, a.getId_usuario());
-           UsuariosDAO.borrar(con, p.getId_usuario());
-           System.out.println("Fin: ");
-        /*SwingUtilities.invokeLater(() -> {
+//          Connection con = ConnectionDB.openConnection();
+//           UbicacionDAO.insertar(con);
+//          
+//           List<Elemento> inv = GestorAlmacenDAO.devolverInventarioCompleto(con);
+//           for(Elemento e: inv){
+//               System.out.println(e);
+//           }
+//           Administrador a=new Administrador("Miguel","miguel25","miguel2525",1);
+//           Profesor p=new Profesor("Saúl","saul25","saul2525",2);
+//           System.out.println("Insertar: ");
+//           UsuariosDAO.insertar(con, a);
+//           UsuariosDAO.insertar(con, p);
+//           System.out.println("Mostrar: ");
+//           HashSet<Usuario> lista=UsuariosDAO.devolverUsuarios(con);
+//           for (Usuario usuario : lista) {
+//               System.out.println(usuario.toString());
+//        }
+//           System.out.println("Borrar: ");
+//           UsuariosDAO.borrar(con, a.getId_usuario());
+//           UsuariosDAO.borrar(con, p.getId_usuario());
+//           System.out.println("Fin: ");
+        SwingUtilities.invokeLater(() -> {
 
-            Interfaz interfaz = new Interfaz();
+            Swing interfaz = new Swing();
             interfaz.setVisible(true);
-        });*/
+        });
     }
 }
